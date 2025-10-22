2cb29dfb225c009423de383b5fd51c10ebdb522a
Akash Bhattacharya <akashbhattacharyak1314@gmail.com>
2025-10-22 22:48:48 +0530

    Add Chapter 1 Value-based animation files

--- app/src/main/java/com/example/jc_animation/Chapter1/Chapter-1-Value-based animations.kt
+++ app/src/main/java/com/example/jc_animation/Chapter1/Chapter-1-Value-based animations.kt
@@ -1,10 +1,95 @@
 package com.example.jc_animation.Chapter1
 
+import android.widget.GridLayout
+import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.spring
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.dp
+
 /**Stage 1: Learn the low-level tools
  * Goal: Animate a box moving across the screen.*/
 @Composable
-fun MovingBox() {
-    
+fun MovingBox(containerWidth: Dp = 400.dp) {
+    //1> state that controls whether box is on right or on left
+    var moved by remember { mutableStateOf(false) }
+    //2)target offset in dp depending on `moved`
+    val targetOffsetDp = if (moved) containerWidth - 50.dp else 0.dp
+    val density = LocalDensity.current
+    //3)animateFloatAsState expects a Float -> convert Dp to px using local density
+    val targetOffsetPx = with(density) { targetOffsetDp.toPx() }
+
+    //4) animateFloatAsState does interpolation for us
+    //underlying values us in Dp (convert to Float by using value in px)
+    // We'll animate using Dp by animating the Float px value
+    //then convert back when setting Modifier.offset
+
+    val animatedOffsetPx by animateFloatAsState(
+        targetValue = targetOffsetPx,
+        //we can change duration/easing by passing animationSpec = tween(500)
+        animationSpec = /*tween(500, easing = LinearOutSlowInEasing)*/
+            spring(dampingRatio = Spring.DampingRatioMediumBouncy,
+                stiffness = Spring.StiffnessLow)
+    )
+
+    Column(
+        modifier = Modifier.padding(16.dp).width(containerWidth),
+        horizontalAlignment = Alignment.Start
+    ) {
+        //Track
+        Box(modifier = Modifier
+            .height(80.dp)
+            .fillMaxWidth()
+            .background(Color(0xFFECEFF1))) {
+            //The animated box
+            Box(modifier = Modifier
+                .offset {
+                    IntOffset(animatedOffsetPx.toInt(), 0)
+                }
+                .size(48.dp)
+                .align(Alignment.CenterStart)
+                .background(Color(0xFF2962FF))
+                .clickable {
+//                    moved = !moved
+                })
+        }
+        Spacer(Modifier.height(12.dp))
+        Button(onClick = {
+            moved = !moved
+        }) {
+            Text(if (moved) "Move left" else "Move right")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun MovingBoxPreview() {
+    MovingBox(containerWidth = 320.dp)
 }
