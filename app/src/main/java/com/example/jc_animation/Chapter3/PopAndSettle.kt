7410f45b0fcab7a85b5f41ef88efce8bc1e1c6f3
Akash Bhattacharya <akashbhattacharyak1314@gmail.com>
2025-11-15 23:20:54 +0530

    Spring effect / Bouncy effect on button

--- app/src/main/java/com/example/jc_animation/Chapter3/PopAndSettle.kt
+++ app/src/main/java/com/example/jc_animation/Chapter3/PopAndSettle.kt
@@ -1,2 +1,107 @@
 package com.example.jc_animation.Chapter3
 
+import androidx.compose.animation.core.FastOutLinearInEasing
+import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.keyframes
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.delay
+
+@Preview
+@Composable
+fun PopButton() {
+    var pressed by remember { mutableStateOf(false) }
+    val scale by animateFloatAsState(
+        targetValue = if (pressed) 1.1f else 1f,
+        animationSpec = keyframes {
+            durationMillis = 200
+
+            //POP
+            1.15f at 60 using FastOutLinearInEasing
+
+            //SETTLE
+            1.0f at 200 using LinearOutSlowInEasing
+        }
+    )
+    LaunchedEffect(pressed) {
+        if (pressed) {
+            delay(200)
+            pressed = false
+        }
+    }
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize().padding(horizontal = 16.dp)
+    ) {
+        Button(onClick = {
+            pressed = true
+        }, modifier = Modifier
+            .graphicsLayer{
+                scaleX = scale
+                scaleY = scale
+            }
+            .fillMaxWidth().height(40.dp)) {
+            Text("Submit")
+        }
+    }
+
+}
+@Preview
+@Composable
+fun SpringPopButton() {
+    var pressed by remember { mutableStateOf(false) }
+    val scale by animateFloatAsState(
+        targetValue = if (pressed) 1.1f else 1f,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow
+        )
+    )
+    LaunchedEffect(pressed) {
+        if (pressed) {
+            delay(200)
+            pressed = false
+        }
+    }
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize().padding(horizontal = 16.dp)
+    ) {
+        Button(onClick = {
+            pressed = true
+        }, modifier = Modifier
+            .graphicsLayer{
+                scaleX = scale
+                scaleY = scale
+            }
+            .fillMaxWidth().height(40.dp)) {
+            Text("Submit")
+        }
+    }
+
+}
